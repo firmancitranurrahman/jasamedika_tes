e99b12d95c4a8258307e8c29ec4dc88fa128bc09
Firman <firmancitranurrahman@gmail.com>
2023-06-17 02:13:33 +0700

    add spatie using role user

---
+++ database/migrations/2023_06_16_185844_create_pasiens_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pasiens', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pasien');
+            $table->string('alamat');
+            $table->string('no_telepon');
+            $table->string('rt_rw');
+            $table->string('kelurahan');
+            $table->date('tgl_lahir');
+            $table->string('jenis_kelamin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pasiens');
+    }
+};
